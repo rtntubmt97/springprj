765a7040f712f4f237ed1898eea2df7d53000d4c
tumd <rtntubmt97@gmail.com>
2021-04-15 18:15:01 +0700

    define node channels

--- node/node.go
+++ node/node.go
@@ -8,12 +8,15 @@ import (
 type Node struct {
 	id        int32
 	connector connectorPkg.Connector
+	channels  map[int32](chan int32)
 }
 
 func (node *Node) Init(id int32) {
 	node.id = id
 	node.connector = connectorPkg.Connector{}
 	node.connector.Init(id)
+
+	node.channels = make(map[int32](chan int32))
 
 	node.connector.SetHandleFunc(define.SendInt32, node.sendInt32_handle)
 	node.connector.SetHandleFunc(define.SendInt64, node.sendInt64_handle)
